16327a537e804b805aba0a1f907f7c7de3d8308d
Eirene Oyakhilome <33825632+Peace99@users.noreply.github.com>
2022-09-14 11:04:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+// async errors
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+
+const connectDB  = require('./database/connect')
+const productsRouter = require('./routes/products')
+
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route </a>')
+})
+
+app.use('/api/v1/products',productsRouter )
+
+//product route
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
